aad5bc60ce9d4141954b5d8eaeb42703a29eb872
shobhitk98 <kshobhit1606@gmail.com>
2024-03-27 23:44:17 +0530

    Completed

---
+++ script.js
@@ -0,0 +1,39 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+// ================================Smoth scrolling loco js============================================
+
+
+
+
+
+
+
+
+
+var tl = gsap.timeline()
+
+tl.to(".page1",{
+    y:"100vh",
+    scale:0.6,
+    duration:0
+})
+tl.to(".page1",{
+    y:"30vh",
+    duration:1,
+    delay:1
+})
+tl.to(".page1",{
+    y:"0vh",
+    rotate:360,
+    scale:1,
+    duration:0.9
+})
+// ===============================Loading Animation======================================
+
+
+
+
+
